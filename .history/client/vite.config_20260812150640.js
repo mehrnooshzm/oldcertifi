313fcbee60499167383ace70d57247670987a7da
mehrnoosh <eng.mehrnoosh.mousavi@gmail.com>
2026-09-09 11:50:49 -0600

    add

--- .history/client/vite.config_20260812150640.js
+++ .history/client/vite.config_20260812150640.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://oldcertifi.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
